8a5f022e632242c54be756e487f738710543768b
Javier Pimás <javierpimas@gmail.com>
2022-08-03 02:12:03 -0300

    more work on bootstrapper

--- bootstrapper.js
+++ bootstrapper.js
@@ -1,45 +1,69 @@
 'use strict';
 
-//import PowertalkLMR from "./interpreter/PowertalkLMR.js";
+import './lmr.js';
+import PowertalkLMR from "./interpreter/PowertalkLMR.js";
 
 import ModuleReader from "./ModuleReader.js"
+
+
 
 let Bootstrapper = class {
 	constructor()
 	{
-		this.modules = {};
-//		this.runtime = new PowertalkLMR();
+		this.modules = new Map;
+		this.runtime = new PowertalkLMR();
 	}
 
-	loadKernel(kernelPath = 'kernel.json')
+	loadKernelFile(kernelPath = 'Kernel.json')
 	{
-		this.kernel = this.loadModule(kernelPath);
+		const module = this.loadModuleFromFile(kernelPath);
+		this.initializeKernel(module);
+	}
+
+	initializeKernel(module)
+	{
+		this.kernel = module;
 		//Object.assign(this.runtime, this.kernel.meta);
 		Object.assign(this.runtime, this.kernelMeta());
-		Object.assign(this.runtime, this.kernelClasses());
+		Object.assign(this.runtime, this.kernelObjects());
+		const symbols = this.kernel.exports["KnownSymbols"]._slots.map(symbol => symbol.asLocalString()._arrow(symbol));
+		this.initializeSymbolTable();
+		this.runtime.knownSymbols_(new Map(Object.entries(symbols)));
+		this.runtime.initializeInterpreter();
 	}
 
-	loadModule(path)
+	initializeSymbolTable()
+	{
+		const symbol = this.kernel.exports["SymbolTable"];
+		this.runtime.symbolTable_(symbol);
+	}
+
+	loadModuleFromFile(path)
 	{
 		const reader = new ModuleReader();
-		return modules[path] = reader.loadFile(path);
+		return this.loadModuleNamed(reader.loadFile(path), path);
 	}
 
-	kernelClasses()
+	loadModuleNamed(module, name)
+	{
+		return this.modules[name] = module;
+	}
+
+	kernelObjects()
 	{
 		return {
-			falseObj: this.kernel.exports["false"],
-			trueObj:  this.kernel.exports["true"],
-			nilObj:   this.kernel.exports["nil"],
-			arrayClass:        this.kernel.exports["Array"],
-			metaclassClass:    this.kernel.exports["Metaclass"],
-			methodClass:       this.kernel.exports["Method"],
-			smallIntegerClass: this.kernel.exports["SmallInteger"],
-			blockClass:        this.kernel.exports["CompiledBlock"],
-			byteArrayClass:    this.kernel.exports["ByteArray"],
-			stringClass:       this.kernel.exports["String"],
-			closureClass:      this.kernel.exports["Closure"],
-			behaviorClass:     this.kernel.exports["Behavior"]
+			_falseObj: this.kernel.exports["false"],
+			_trueObj:  this.kernel.exports["true"],
+			_nilObj:   this.kernel.exports["nil"],
+			_arrayClass:        this.kernel.exports["Array"],
+			_metaclassClass:    this.kernel.exports["Metaclass"],
+			_methodClass:       this.kernel.exports["CompiledMethod"],
+			_smallIntegerClass: this.kernel.exports["SmallInteger"],
+			_blockClass:        this.kernel.exports["CompiledBlock"],
+			_byteArrayClass:    this.kernel.exports["ByteArray"],
+			_stringClass:       this.kernel.exports["String"],
+			_closureClass:      this.kernel.exports["Closure"],
+			_behaviorClass:     this.kernel.exports["Behavior"]
 		};
 	}
 
@@ -49,29 +73,29 @@ let Bootstrapper = class {
 //
 	kernelMeta () {
 		return {
-			wordSize: 8,
-			behaviorNextIndex: 3,
-			behaviorMethodDictionaryIndex: 2,
-			behaviorClassIndex: 1,
-			classNameIndex: 6,
-			methodFlagsIndex: 1,
-			maxSMI: 4611686018427387903,
-			minSMI:-4611686018427387904,
-			speciesInstanceBehaviorIndex: 2,
-			speciesFormatIndex: 3,
-			methodOptimizedCodeIndex: 2,
-			methodAstcodesIndex: 3,
-			methodInstSize: 6,
-			methodClassBindingIndex: 4,
-			speciesSuperclassIndex: 1,
-			speciesIvarsIndex: 5,
-			dictionaryTableIndex: 2,
-			classClassVariablesIndex: 8,
-			metaclassInstanceClassIndex: 6,
-			classModuleIndex: 10,
-			moduleNamespaceIndex: 4,
-			closureBlockIndex: 1,
-			blockMethodIndex: 3
+			_wordSize: 8,
+			_behaviorNextIndex: 3,
+			_behaviorMethodDictionaryIndex: 2,
+			_behaviorClassIndex: 1,
+			_classNameIndex: 6,
+			_methodFlagsIndex: 1,
+			_maxSMI: BigInt("4611686018427387903"),
+			_minSMI: BigInt("-4611686018427387904"),
+			_speciesInstanceBehaviorIndex: 2,
+			_speciesFormatIndex: 3,
+			_methodOptimizedCodeIndex: 2,
+			_methodAstcodesIndex: 3,
+			_methodInstSize: 6,
+			_methodClassBindingIndex: 4,
+			_speciesSuperclassIndex: 1,
+			_speciesIvarsIndex: 5,
+			_dictionaryTableIndex: 2,
+			_classClassVariablesIndex: 8,
+			_metaclassInstanceClassIndex: 6,
+			_classModuleIndex: 10,
+			_moduleNamespaceIndex: 4,
+			_closureBlockIndex: 1,
+			_blockMethodIndex: 3
 		};
 	}
 }

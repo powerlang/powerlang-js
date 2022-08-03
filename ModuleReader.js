0cdf4717423802863fcbff915496c916e5388d94
Javier Pimás <javierpimas@gmail.com>
2022-08-03 02:13:21 -0300

    adapt ModuleReader to more compact json format

--- ModuleReader.js
+++ ModuleReader.js
@@ -7,11 +7,27 @@ import LMRSmallInteger from './interpreter/LMRSmallInteger.js';
 import { readFileSync } from 'fs';
 
 export const ObjectTypes = Object.freeze({
-	LMRSlotObject: 1,
-	LMRByteObject: 2,
-	LMRSmallIntegerObject: 3,
+	SlotObject: 1,
+	ByteObject: 2,
+	SmallIntegerObject: 3,
 	Import: 4
 })
+
+/**
+ * @param {*} environment is a dictionary mapping names to objects, that is used for module imports
+ *
+ * A ModuleReader is an object that creates a Powertalk module by reading it from a JSON file.
+ * The format of the module is pretty simple: it has an object table, and an export map.
+ * Objects in the table can be of four types: SmallInteger, ByteObject, SlotObject and Import
+ *  - Import objects have a name, which is used to find the corresponding object passed in
+ *    the environment dictionary.
+ *  - Small integers are immediates
+ *  - Byte objects have bytes, hash and behavior
+ *  - Slot objects have slots, hash and behavior.
+ * Both slots and behaviors are stored as indexes in the object table.
+ * The reader generates all object instances in a first pass, and then changes references to
+ * other objects by accessing the corresponding index in the object table.
+ */
 
 const ModuleReader = class {
     constructor(environment = {})
@@ -22,13 +38,14 @@ const ModuleReader = class {
    	loadFile(path) {
 		let rawdata = readFileSync(path);
 		let module = JSON.parse(rawdata);
-		loadObjects(module);
+		this.loadObjects(module);
+		return this;
 	}
 
 	loadObjects(module) {
 		this.objects = module.objects.map(obj => this.recreate(obj));
 		this.objects.forEach(obj => this.linkSlots(obj));
-		this.exports = Object.fromEntries(Object.entries(module.exports).map( ([name, index]) => [name, this.objects[index]]));
+		this.exports = Object.fromEntries(module.exports.map( exp => [exp[0], this.objects[exp[1]]]));
 		return this.objects;
 	}
 
@@ -38,12 +55,12 @@ const ModuleReader = class {
 	}
 
 	recreate(object) {
-		switch (object.type) {
-			case ObjectTypes.LMRSlotObject:
+		switch (this.objectType(object)) {
+			case ObjectTypes.SlotObject:
 				return this.newSlotObject(object);
-			case ObjectTypes.LMRByteObject:
+			case ObjectTypes.ByteObject:
 				return this.newByteObject(object);
-			case ObjectTypes.LMRSmallIntegerObject:
+			case ObjectTypes.SmallIntegerObject:
 				return this.newSmallInteger(object);
             case ObjectTypes.Import:
                 return this.environment[object.name];
@@ -55,27 +72,27 @@ const ModuleReader = class {
 	newSlotObject(object)
 	{
 		let result = new LMRSlotObject();
-		result.slots = object.slots;
-		result.header = new LMRObjectHeader();
-		result.header.behavior = object.behavior;		
-		result.header.hash = object.hash;
+		result._slots = this.objectSlots(object);
+		result._header = new LMRObjectHeader();
+		result._header._behavior = this.objectBehavior(object);
+		result._header._hash = this.objectHash(object);
 		return result;
 	}
 
 	newByteObject(object)
 	{
 		let result = new LMRByteObject();
-		result.bytes = object.bytes;
-		result.header = new LMRObjectHeader();
-		result.header.behavior = object.behavior;		
-		result.header.hash = object.hash;
+		result._bytes = this.objectBytes(object);
+		result._header = new LMRObjectHeader();
+		result._header._behavior = this.objectBehavior(object);
+		result._header._hash = this.objectHash(object);
 		return result;
 	}
 
 	newSmallInteger(object)
 	{
 		let result = new LMRSmallInteger();
-		result.value = object.value;
+		result._value = object[1];
 		return result;
 	}
 	
@@ -83,11 +100,42 @@ const ModuleReader = class {
 		if (object.isImmediate())
 			return;
 		
-		object.header.behavior = this.objects[object.header.behavior];
+		object._header.behavior = this.objects[object._header._behavior];
 		
 		if (!object.isBytes())
-			object.slots = object.slots.map(index => this.objects[index]);
+			object._slots = object._slots.map(index => this.objects[index]);
 	}
+
+	objectType(object)
+	{
+		return object[0];
+	}
+
+	objectSlots(object)
+	{
+		return object[1];
+	}
+
+	objectBytes(object)
+	{
+		return object[1];
+	}
+
+	objectHeader(object)
+	{
+		return object[2];
+	}
+
+	objectBehavior(object)
+	{
+		return this.objectHeader(object)[0];
+	}
+
+	objectHash(object)
+	{
+		return this.objectHeader(object)[1];
+	}
+
 }
 
 export default ModuleReader
